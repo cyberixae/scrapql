f0e28c36d4e73015cdfe488aff3f1670d6ee6a55
Toni Ruottu <toni.ruottu@maas.global>
2020-01-15 13:01:07 +0200

    moor

--- src/tuple.ts
+++ src/tuple.ts
@@ -12,26 +12,13 @@ const foo: Open<'foo', Empty> = open<'foo', Empty>('foo');
 const bar: Open<'bar', Empty> = open<'bar', Empty>('bar');
 const lol: Open<'lol', Empty> = open<'lol', Empty>('lol');
 
+export type Plus<A, B, L> = <N extends Tuple<any, any, L>>(n: N) => Tuple<A, Tuple<B, N, L>, L>
+export const plus = <A, B, L>(a: Open<A, L>, b: Open<B, L>): Plus<A, B, L> => <N extends L|Tuple<Tuple<any, any, L>, any, L>>(n: N) => a(b(n))
 
-const muumi = <N extends Empty|Tuple<any, any, Empty>>(n: N) => foo(bar(n))
-
-// type Plus = (a: Open<>, b: Open<>) => Open<>
-
-
-export type Plus<A extends Open<AC, L>, B extends Open<BC, L>, AC, BC, L> = <N extends Tuple<any, any, L>>(n: N) => Tuple<AC, Tuple<BC, N, L>, L>
-export const plus = <A extends Open<AC, L>, B extends Open<BC, L>, AC, BC, L>(a: A, b: B): Plus<A, B, AC, BC, L> => <N extends L|Tuple<Tuple<any, any, L>, any, L>>(n: N) => a(b(n))
-
-
-
-
-
-/*
-export type Open<C, L> = <N extends L|Tuple<any, any, L>>(n: N) => Open<N, L>
-export const open = <C, N extends L|Tuple<any, any, L>, L>(c: C) => (n: N): Tuple<C, N, L> => tuple(c, n)
-*/
-
+const muumi1: Plus<'foo', 'bar', Empty> = (n) => foo(bar(n))
+const muumi2: Plus<'foo', 'bar', Empty> = (n) => foo(bar(n))
 
 export type Closed<C, N extends Empty|Closed<any, any>> = Tuple<C, N, Empty>
-export const closed = (t: Open) =>
+export const closed = <C, L>(t: Open<C, L>, l: L): Tuple<C, L, L> => t(l)
 
 export {}
