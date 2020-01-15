0f8e9935209d4db3cc75733899754849fb7e6d62
Toni Ruottu <toni.ruottu@maas.global>
2020-01-15 12:44:11 +0200

    omgomg

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,15 +1,37 @@
-import { Prepend, Concat, Reverse } from 'typescript-tuple';
 
-/* eslint-disable fp/no-mutating-methods */
+export type Tuple<C, N extends Tuple<any, any, L>, L> = L|[C, N]
+export const tuple = <C, N extends L|Tuple<any, any, L>, L>(c: C, n: N): Tuple<C, N, L> => [c, n]
 
-export type Tuple<S = any> = Array<S>;
+export type Empty = Array<never>
+export const empty: Empty = []
 
-export const concat = <X extends Tuple>(x: X) => <T extends Tuple>(
-  tuple: T,
-): Concat<T, X> => (tuple.concat(x) as unknown) as Concat<T, X>;
+export type Open<C, L> = <N extends L|Tuple<any, any, L>>(n: N) => Tuple<C, N, L>
+export const open = <C, L>(c: C): Open<C, L> => <N extends L|Tuple<any, any, L>>(n: N): Tuple<C, N, L> => tuple(c, n)
 
-export const prepend = <X>(x: X) => <T extends Tuple>(tuple: T): Prepend<T, X> =>
-  [x, ...tuple] as Prepend<T, X>;
+const foo: Open<'foo', Empty> = open<'foo', Empty>('foo');
+const bar: Open<'bar', Empty> = open<'bar', Empty>('bar');
+const lol: Open<'lol', Empty> = open<'lol', Empty>('lol');
 
-export const reverse = <T extends Tuple>(tuple: T): Reverse<T> =>
-  tuple.reverse() as Reverse<T>;
+
+const muumi = <N extends Empty|Tuple<any, any, Empty>>(n: N) => foo(bar(n))
+
+// type Plus = (a: Open<>, b: Open<>) => Open<>
+
+
+export type Plus<A extends Open<AC, L>, B extends Open<BC, L>, AC, BC, L> = <N extends Tuple<any, any, L>>(n: N) => Tuple<AC, Tuple<BC, N, L>, L>
+export const plus = <A extends Open<AC, L>, B extends Open<BC, L>, AC, BC, L>(a: A, b: B): Plus<A, B, AC, BC, L> => <N extends L|Tuple<Tuple<any, any, L>, any, L>>(n: N) => a(b(n))
+
+
+
+
+
+/*
+export type Open<C, L> = <N extends L|Tuple<any, any, L>>(n: N) => Open<N, L>
+export const open = <C, N extends L|Tuple<any, any, L>, L>(c: C) => (n: N): Tuple<C, N, L> => tuple(c, n)
+*/
+
+
+export type Closed<C, N extends Empty|Closed<any, any>> = Tuple<C, N, Empty>
+export const closed = (t: Open) =>
+
+export {}
